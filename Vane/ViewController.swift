a7ad8b711d406f0d6c490c236d6a00eb4a2331ca
Andrii Antosha <indieamindie@gmail.com>
2020-04-07 17:16:25 +0200

    Initial Commit

---
+++ Vane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vane
+//
+//  Created by Andrey Antosha on 07/04/2020.
+//  Copyright © 2020 Andrey Antosha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
